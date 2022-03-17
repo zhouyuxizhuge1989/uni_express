5c6d70d824a23551a736b594e6809118214b2891
haosijia <haosijia@jd.com>
2022-03-17 18:36:20 +0800

    Initial commit

---
+++ src/main/java/org/fly/uni/express/App.java
@@ -0,0 +1,13 @@
+package org.fly.uni.express;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
